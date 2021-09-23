1e963f58da5e6d7fb7a36f50dc7f764a033c3c0a
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 22:10:19 -0400

    Separate TCP connection module

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require("net");
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: '50542'// PORT number here,
-  });
+const { connect } = require('./client');
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  // display received data event handler
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
