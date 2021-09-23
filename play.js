c52a7f7662b6fd9ce4030f27e2e6e548cd8cf289
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 22:44:34 -0400

    Separate input handling module from play.js

--- play.js
+++ play.js
@@ -1,25 +1,7 @@
-const { connect } = require('./client');
-
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-const input = setupInput();
-input.on("data", handleUserInput);
+setupInput();
