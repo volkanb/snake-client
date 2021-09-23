dd86bb283a153531e989e2ae7a1eced2e7408d8f
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 22:34:20 -0400

    Adds player input handling for termination

--- play.js
+++ play.js
@@ -3,3 +3,23 @@ const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const input = setupInput();
+input.on("data", handleUserInput);
