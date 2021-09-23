b2dfaefab853889e4054bc969ae457716393393e
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 22:56:32 -0400

    Send snake movement key data to server

--- input.js
+++ input.js
@@ -1,13 +1,23 @@
+let connection;
 // handle user input
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
-  }
+  } else if (key === 'w') {
+    connection.write("Move: up");
+  } else if (key === 's') {
+    connection.write("Move: down");
+  } else if (key === 'd') {
+    connection.write("Move: right");
+  } else if (key === 'a') {
+    connection.write("Move: left");
+  }  
 };
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
