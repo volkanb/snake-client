2134fc9910ddfb0086f858e196b92da90dfb50fd
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 23:05:52 -0400

    Adds/integrates constants

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
+const { IP, PORT } = require("./constants");
 const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: '50542'// PORT number here,
+    host: IP,// IP address here,
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
