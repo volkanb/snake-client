97d40b85908bc21e9bde26711988de609883bccd
Volkan Benli <volkan.benli@lighthouselabs.com>
2021-09-22 22:09:01 -0400

    First commit

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require("net");
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',// IP address here,
+    port: '50542'// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // display received data event handler
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
